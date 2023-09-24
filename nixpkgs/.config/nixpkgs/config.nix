a381c2b195d0f1cc5cb0ce6bab535535bfbf4a43
Konstantinos Nalpa~ <coder@terraix.com>
2023-09-24 12:54:14 +0300

    Add nixpkgs directory

---
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -0,0 +1,30 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        jq
+        ripgrep
+        fd
+        tmux
+        neovim
+        git
+        tig
+        fzf
+        ncdu
+        autojump
+        stow
+        tldr
+        ansible
+        yt-dlp
+        ansible-lint
+        yamllint
+        pipx
+        hugo
+        starship
+        pipx
+        drawio
+      ];
+    };
+  };
+}
